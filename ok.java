4cb773b8c0d863834903cc8c6ab5824e36dd254f
llaryall <78265295+llaryall@users.noreply.github.com>
2021-05-24 17:57:31 +0430

    just uploading the project

--- ok.java
+++ ok.java
@@ -1,1 +1,28 @@
-import java.util.nextInt;
+import java.util.Scanner;
+public class ok {
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+       System.out.println("enter your array size : "); int m =input.nextInt();
+        int[] array = new int[m];
+        for(int i = 0 ; i<5 ; i++){
+           System.out.println("enter the "+i+"th number"); array[i]= input.nextInt();}
+        clearing(array);
+    }
+    static void clearing(int[]b){
+        int min = b[0]; int max = b[0]; float sum=0;
+        for(int i = 1 ; i<b.length ; i++)
+          if(b[i]<min)
+              min=b[i];
+        for(int i = 1 ; i<b.length ; i++)
+          if(b[i]>min)
+              min=b[i];
+        for(int i = 0; i<b.length ; i++){
+          sum +=b[i];}
+        float avrage = sum/b.length;
+        
+
+        
+        System.out.println("the max number is : "+max+" the min number is : "+ min+" and the avrage is : "+avrage);
+    } 
+}
